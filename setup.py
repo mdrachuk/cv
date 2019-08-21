1acb3e0fdf4c8ddd5241d546d5d237ca9af7d656
mdrachuk <misha@drach.uk>
2019-08-21 17:23:33 +0300

    Bump serious version

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     keywords="python packaging version pypi ci",
     python_requires=">=3.7",
     scripts=['cv'],
+    zip_safe=False,
     project_urls={
         'Pipelines': 'https://dev.azure.com/misha-drachuk/cv',
         'Source': 'https://github.com/mdrachuk/cv/',
@@ -29,7 +30,7 @@ setup(
     },
     install_requires=[
         'requests>=2.22.0',
-        'serious==1.0.0.dev18',
+        'serious==1.0.0.dev20',
     ],
     classifiers=[
         "Intended Audience :: Developers",
